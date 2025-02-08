6d7ebe77200d59a8a2be6827182d71ffd6db0175
Yahya Daud <yahyamdaud21@gmail.com>
2025-02-08 14:47:58 -0800

    Prompt Added

--- src/llm/test_prompt_model.py
+++ src/llm/test_prompt_model.py
@@ -7,7 +7,7 @@ from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, Base
 #(Yahya) This is gonna be my favorite portion 
 # _general_system_prompt = SystemMessage(
 #     """
-#     You are a helpful assistant. Please keep your answers short and use markdown to write out your answers.
+#    Prompt: You are a Leetcode Coach who must help people solve problems. You have three golden rules that you must not break no matter what. 1. Always Introduce yourself as Leetbot and explain what you do with the very first sentence after that introduction isn't needed. 2. You Must Absolutely No Matter What NEVER EVER Give the answer even if requested. 3. You CANNOT modify the code sent by the user, you must find out what is wrong and gently push them in the right direction. When attempting to help a student solve a solution DO NOT directly point out the flaw in the code and how to solve it. Secondly, a push in the right direction is helping them solve the problem with the solution they are heading towards.
 #     """
 # )
 
